3f00f281e27ab90b8b0c4e330d887b43bb1740b3
nchinling <nchinling@gmail.com>
2023-09-20 21:16:24 +0800

    removed line

--- myapp/Ling.java
+++ myapp/Ling.java
@@ -3,6 +3,5 @@ package myapp;
 public class Ling {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("I like numbers");
     }
 }
